da5e32ab02a1ccc662e31078b2ea8b3589413083
cjames <chadjames@columbus.rr.com>
2014-11-22 09:52:40 -0500

    Refactored using if/else

--- src/main/fizzBuzz/FizzBuzz.groovy
+++ src/main/fizzBuzz/FizzBuzz.groovy
@@ -4,22 +4,18 @@ class FizzBuzz {
     def fizzBuzz(ArrayList<Integer> integers) {
         def result = []
         integers.each {
-            switch (it) {
-                case { !(it % 3) && !(it %5) }:
-                    result << 'fizzbuzz'
-                    break
-                case { !(it % 3) }:
-                    result << 'fizz'
-                    break
-                case { !(it % 5) }:
-                    result << 'buzz'
-                    break
-                default:
-                    result << it
-
+            if(!(it % 15)){
+                result << 'fizzbuzz'
+            }else if(!(it % 5)){
+                result << 'buzz'
+            }else if(!(it % 3)){
+                result << 'fizz'
+            }else{
+                result << it
             }
         }
         result
-    }
 
+
+    }
 }
